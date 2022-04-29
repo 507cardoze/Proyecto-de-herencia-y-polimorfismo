ba23d1cbac119db7ef498be28d9fe62b746acf0e
507cardoze <anthonycardoze@gmail.com>
2022-04-28 19:02:04 -0500

    proyecto terminado

--- src/com/anthony/Main.java
+++ src/com/anthony/Main.java
@@ -19,16 +19,17 @@ public class Main {
         int getPuntaje();
         void setPuntaje(int a);
         void setNombre(String name);
+        String getNombre();
     }
 
 
     public static class MotorJuego implements motor {
-        protected int turno = 0;
+        protected int turno = 1;
         protected boolean ejecutado = true;
         protected int confirm = 0;
         Random ranNum = new Random();
         public void incrementTurno() {
-            turno = turno++;
+            turno = turno + 1;
         }
         public int getTurno(){
             return turno;
@@ -61,51 +62,67 @@ public class Main {
             return puntaje;
         }
         public void setPuntaje(int a) {
-            puntaje = a;
+            puntaje =  puntaje + a;
         }
         public void setNombre(String name) { nombreJugador = name;}
+        public String getNombre(){return nombreJugador;}
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         MotorJuego juego = new MotorJuego();
         Jugador player1 = new Jugador();
         Jugador player2 = new Jugador();
         Scanner sc = new Scanner(System.in);
-        int confirm = 0;
+        int buffer;
 
         System.out.println("---- Juegos de azar ----");
         while(!(player1.nombreJugador.length() > 0)){
             System.out.println("Ingrese el nombre del jugador 1: ");
             player1.setNombre(sc.nextLine());
         }
-
+        System.out.println("---------------------------------------------------------------");
         while(!(player2.nombreJugador.length() > 0)){
             System.out.println("Ingrese el nombre del jugador 2: ");
             player2.setNombre(sc.nextLine());
         }
-
-        while(juego.isEjecutado() == true){
+        System.out.println("---------------------------------------------------------------");
+        while(juego.isEjecutado()){
             System.out.println("turno # " + juego.getTurno());
-            switch(juego.getTurno() % 2){
+            System.out.println("---------------------------------------------------------------");
+            juego.incrementTurno();
 
-                case 0: {
-
-                }
-
-                default: break;
-            }
-
-            if(juego.getTurno() % 2 == 0){
-
-                System.out.println("Desea seguir jugado? 1 para continuar, 2 para detenerse");
-                confirm = sc.nextInt();
-
-                if(confirm == 2){
-                    juego.detenerEjecucion();
-                }
+            if(juego.getTurno() % 2 == 0 && juego.getTurno() != 1){
+                buffer = juego.lanzarDados();
+                player1.setPuntaje(buffer);
+                System.out.println("Jugando dados.... ");
+                System.out.println("salio: " + buffer);
             }else{
-                juego.incrementTurno();
+                buffer = juego.lanzarDados();
+                player2.setPuntaje(buffer);
+                System.out.println("Jugando dados.... ");
+                System.out.println("salio: " + buffer);
             }
+            System.out.println("---------------------------------------------------------------");
+            System.out.println("Puntaje de Jugador 1: "+ player1.getNombre() + " | " + player1.getPuntaje());
+            System.out.println("Puntaje de Jugador 2: "+ player2.getNombre() + " | " + player2.getPuntaje());
+            System.out.println("---------------------------------------------------------------");
+            System.out.println("---------------------------------------------------------------");
+            System.out.println("Desea seguir jugado? 1 para continuar, 2 para detenerse");
+            System.out.println("---------------------------------------------------------------");
+            System.out.println("---------------------------------------------------------------");
+            juego.setConfirm(sc.nextInt());
+
+            if(juego.getConfirm() == 2){
+                juego.detenerEjecucion();
+            }
+        }
+
+        if(player1.getPuntaje() == player2.getPuntaje()){
+            System.out.println("Es un empate.");
+        }else if (player1.getPuntaje() > player2.getPuntaje()){
+            System.out.println("El ganador es: " + player1.getNombre());
+        }else{
+            System.out.println("El ganador es: " + player2.getNombre());
         }
 
         System.out.println("---- Muchas gracias ----");
